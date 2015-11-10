0173d2a116dc2fa5050e9a499e02e940117c3ad1
troll <troll@puppetlabs.vm>
2015-11-10 19:40:21 +0000

    needs to be run by shell

--- modules/review/manifests/init.pp
+++ modules/review/manifests/init.pp
@@ -32,7 +32,7 @@ class review (
 
   file_line { '/etc/profile':
     path  => '/etc/profile',
-    line  => '/usr/local/bin/dynmotd >| /etc/motd',
+    line  => 'sh /usr/local/bin/dynmotd >| /etc/motd',
   }
 
   user { $user:
