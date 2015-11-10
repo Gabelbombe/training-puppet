d5a2a0b39d05b529d54218d34be9cdfcb66dc25b
troll <troll@puppetlabs.vm>
2015-11-10 19:36:04 +0000

    adding broken modify motd

--- modules/review/manifests/init.pp
+++ modules/review/manifests/init.pp
@@ -1,4 +1,4 @@
-class review(
+class review (
   $user = 'review'
 ) {
   file { '/etc/shells':
@@ -9,12 +9,30 @@ class review(
     source  => 'puppet:///modules/review/shells',
   }
 
-  file { '/etc/motd':
+#  file { '/etc/motd':
+#    ensure  => file,
+#    owner   => 'root',
+#    group   => 'root',
+#    mode    => '0644',
+#    content => template('review/motd.erb'),
+#  }
+
+  file { '/usr/local/bin/dynmotd':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
-    mode    => '0644',
+    mode    => '0755',
     content => template('review/motd.erb'),
+  }
+
+  file_line { '/pam/login':
+     path   => '/etc/pam.d/login',
+     line   => 'session    optional     pam_motd.so',
+  }
+
+  file_line { '/etc/profile':
+    path  => '/etc/profile',
+    line  => '/usr/local/bin/dynmotd',
   }
 
   user { $user:
