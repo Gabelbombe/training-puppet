50608ce784b53a2087493451a2b3d0812d439d83
troll <troll@puppetlabs.vm>
2015-11-10 21:14:53 +0000

    adding what i think they want for hosts

---
+++ modules/system/manifests/hosts.pp
@@ -0,0 +1,25 @@
+class hosts {
+  $hosts = {
+    'localhost'             => {
+      host_aliases => ['localhost.localdomain', 'localhost6', 'localhost6.localdomain6'],
+      ip           => '::1',
+    },
+    'master.puppetlabs.vm'  => {
+      host_aliases => ['master'],
+      ip           => '10.0.20.3',
+    },
+    'student.puppetlabs.vm' => {
+      host_aliases => ['training', 'localhost', 'localhost.localdomain', 'localhost4'],
+      ip           => '127.0.0.1',
+    },
+    'troll.puppetlabs.vm'   => {
+      host_aliases => ['troll'],
+      ip           => '10.0.2.15',
+    },
+  }
+  Hosts {
+    ensure => present,
+    target => '/etc/hosts',
+  }
+  create_resources( 'host', $hosts )
+}
