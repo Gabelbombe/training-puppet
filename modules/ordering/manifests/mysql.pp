649c965470ea9ace3fd42dbffa46db3bd20f4ca1
troll <troll@puppetlabs.vm>
2015-11-10 23:18:10 +0000

    adding mysql

---
+++ modules/ordering/manifests/mysql.pp
@@ -0,0 +1,22 @@
+class ordering::mysql {
+
+  anchor { 'ordering::mysql::begin': }
+  ->
+  class { '::mysql::server':
+    root_password => 'strongpassword', }
+  ->
+  anchor { 'ordering::mysql::end': }
+  class { 'mysql::bindings':
+    php_enable    => true,
+    perl_enable   => true,
+  }
+
+  #contain mysql::bindings
+  # If you didn't want to use anchors, you could also call the contain()
+  # function like this. Like the include() function, it is idempotent, so
+  # the fact that we have declared it above is not a problem. That *would* be
+  # parse order dependent though. If we were to put the contain() function
+  # before the class declaration, the compile would fail with a duplicate
+  # resource declaration error. #
+  # contain mysql::server
+}
