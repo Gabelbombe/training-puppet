e542997b57cfb88f67356cfb8c4494a3ca8df319
troll <troll@puppetlabs.vm>
2015-11-11 16:55:02 +0000

    modified mysql

--- modules/ordering/manifests/mysql.pp
+++ modules/ordering/manifests/mysql.pp
@@ -1,16 +1,27 @@
 class ordering::mysql {
 
-  anchor { 'ordering::mysql::begin': }
-  ->
-
   class { '::mysql::server':
-    root_password => '4!;?:^M]A{}1wE|#{ _|.G}"~B%<?Vph', }
-  ->
-
-  anchor { 'ordering::mysql::end': }
-
-  class { 'mysql::bindings':
+    root_password => '4!;?:^M]A{}1wE|#{ _|.G}"~B%<?Vph',
+  } ->
+  class { '::mysql::bindings':
     php_enable    => true,
     perl_enable   => true,
-  }
+  } ->
+  anchor { 'mysql::end': }
+
+
+## old
+#  anchor { 'ordering::mysql::begin': }
+#  ->
+#
+#  class { '::mysql::server':
+#    root_password => '4!;?:^M]A{}1wE|#{ _|.G}"~B%<?Vph', }
+#  ->
+#
+#  anchor { 'ordering::mysql::end': }
+#
+#  class { 'mysql::bindings':
+#    php_enable    => true,
+#    perl_enable   => true,
+#  }
 }
