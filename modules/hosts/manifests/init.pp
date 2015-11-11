ef009650fa3f8633f932708f9afca345112924b4
troll <troll@puppetlabs.vm>
2015-11-11 17:38:17 +0000

    dep

--- modules/hosts/manifests/init.pp
+++ modules/hosts/manifests/init.pp
@@ -1,25 +1,5 @@
 class hosts {
 
-#  $hosts = {
-#    'master.puppetlabs.vm'  => {
-#      host_aliases => ['master'],
-#      ip           => '10.0.20.3',
-#    },
-#    'localhost'             => {
-#      host_aliases => ['localhost.localdomain', 'localhost6', 'localhost6.localdomain6'],
-#      ip           => '::1',
-#    },
-#    'student.puppetlabs.vm' => {
-#      host_aliases => ['training', 'localhost', 'localhost.localdomain', 'localhost4'],
-#      ip           => '127.0.0.1',
-#    },
-#
-#    $::fqdn   => {
-#      host_aliases => [$::hostname],
-#      ip           => $::ipaddress,       ## '10.0.2.15',
-#    },
-#  }
-
   @@host { $::fqdn:
     host_aliases    => [ $::hostname ],
     ip              => $::ipaddress,
@@ -31,7 +11,6 @@ class hosts {
     target => '/etc/hosts',
   }
 
-#  create_resources( 'host', $hosts )
 }
 
 
