7f809485a4190ac22e50bee0d9925a6b76ebef98
troll <troll@puppetlabs.vm>
2015-11-11 20:05:06 +0000

    updating kerberos to use augeas

--- modules/kerberos/lib/facter/default_realm.rb
+++ modules/kerberos/lib/facter/default_realm.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/ruby -w
+require 'augeas'
 Facter.add("default_realm") do
   setcode("/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf")
+end
+
+Augeas::open do | aug |
+  aug.get('/files/etc/krb5.conf/libdefaults/default_realm')
 end
---
+++ modules/kerberos/manifests/init.pp
@@ -0,0 +1,5 @@
+class kerberos {
+  augeas { 'krb5.conf':
+    context => '/files/etc/krb5.conf/libdefaults',
+    changes => 'set default_realm PUPPETLABS.VM', }
+}
