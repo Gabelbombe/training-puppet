0b8a26d85b6652bd02d2a470e1d262491908987b
troll <troll@puppetlabs.vm>
2015-11-11 19:28:54 +0000

    adding file module

--- modules/files/manifests/init.pp
+++ modules/files/manifests/init.pp
@@ -1,3 +1,35 @@
 class files {
+  $motd = '/etc/motd'
 
+  file { '/etc/cron.allow': ensure  => file }
+
+  file_line { 'prevent cron jobs':
+    path => '/etc/cron.deny',
+    ensure => present,
+    line => '*',
+  }
+
+  file_line { 'allow from root':
+    path => '/etc/cron.allow',
+    ensure => present,
+    line => 'root',
+  }
+
+  concat { $motd:
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
+  concat::fragment { 'motd header':
+    content => template('files/header.erb'),
+    target => $motd,
+    order => 1,
+  }
+
+  concat::fragment { 'motd footer':
+    content => template('files/footer.erb'),
+    target => $motd,
+    order => 999,
+  }
 }
