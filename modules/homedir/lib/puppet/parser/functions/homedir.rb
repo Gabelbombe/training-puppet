6f9dca8245b59bdb78609ea3fd91aacc9b903185
troll <troll@puppetlabs.vm>
2015-11-11 18:45:46 +0000

    wont use ifs for some reason

--- modules/homedir/lib/puppet/parser/functions/homedir.rb
+++ modules/homedir/lib/puppet/parser/functions/homedir.rb
@@ -1,17 +1,14 @@
 #!/usr/bin/ruby -w
 Puppet::Parser::Functions.newfunction(:homedir,
+    :doc      => "Returns the conventional Linux home directory of a username.",
     :type     => :rvalue,
-    :arity    => 1,
-    :doc      => "Returns the conventional Linux home directory of a username."
+    :arity    => 1
   ) do | args |
 
-  user = args[0]
-  raise ArgumentError, "Expects a string" unless user.class == String
+  user = args[0] ; raise ArgumentError, "Expects a string" unless user.class == String
 
 case user
-  when 'root'
-    '/root'
-  else
-    "/home/#{user}"
-  end
+    when 'root' '/root'
+    else "/home/#{user}"
+    end
 end
