5b4e1540f35c1598203a82b226c3b94a3f612877
troll <troll@puppetlabs.vm>
2015-11-11 18:57:03 +0000

    i was bored and did some list user stuff in ruby

--- modules/homedir/lib/puppet/parser/functions/homedir.rb
+++ modules/homedir/lib/puppet/parser/functions/homedir.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/ruby -w
-Puppet::Parser::Functions.newfunction(:homedir,
+Puppet::Parser::Functions.newfunction(
+    :homedir,
     :doc      => "Returns the conventional Linux home directory of a username.",
     :type     => :rvalue,
     :arity    => 1
   ) do | args |
 
   user = args[0] ; raise ArgumentError, "Expects a string" unless user.class == String
+
+# shows all users on the system, will run again per user called in homedir.pp
+#    users = `cat /etc/passwd |grep '/home'`
+#    users.split("\n").select { | user |
+#        puts "-> #{user.split(':')[0]}"
+#    }
 
 case user
     when 'root' '/root'
