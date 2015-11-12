83ead5225dbc21207988d79b185e97ebcafd07be
troll <troll@puppetlabs.vm>
2015-11-12 17:51:38 +0000

    fixed

--- modules/webapp/manifests/wordpress.pp
+++ modules/webapp/manifests/wordpress.pp
@@ -1,6 +1,4 @@
 class role::wordpress {
-  include profile::wordpress
-  include profile::security::base
-  include profile::security::web_app
-  include profile::base::linux
+
+  include webapp  class { '::wordpress': install_dir => hiera('webapp::docroot') }
 }
